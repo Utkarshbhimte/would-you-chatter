02559db1ede816a8c654ccc40fe346a2ecd5e0e0
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2018-03-21 19:51:56 +0530

    added sample data

---
+++ src/components/sampleData.js
@@ -0,0 +1,31 @@
+export const messages = [
+  {
+    type: "text",
+    text:
+      "asdasa sdaio j oisjd oasdj aoisdjoai sdoa sdaio doias dioasjd oasjs doias",
+    sender: "me"
+  },
+  {
+    type: "text",
+    text:
+      "asdasa sdaio j oisjd oasdj aoisdjoai sdoa sdaio doias dioasjd oasjs doias",
+    sender: ""
+  },
+  {
+    type: "text",
+    text:
+      "asdasa sdaio j oisjd oasdj aoisdjoai sdoa sdaio doias dioasjd oasjs doias",
+    sender: ""
+  },
+  {
+    type: "question",
+    options: ["die in a car crash", "die while drowning"],
+    sender: ""
+  },
+  {
+    type: "text",
+    text:
+      "asdasa sdaio j oisjd oasdj aoisdjoai sdoa sdaio doias dioasjd oasjs doias",
+    sender: ""
+  }
+];
